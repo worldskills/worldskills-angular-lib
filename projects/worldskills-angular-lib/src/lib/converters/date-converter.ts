4750224e09b754d880fd494c5f7f4ed1a3a2b4d4
waseem.sabjee <waseem.sabjee@worldskills.org>
2019-02-28 07:38:46 +1000

    Improved Date Converter

--- projects/worldskills-angular-lib/src/lib/converters/date-converter.ts
+++ projects/worldskills-angular-lib/src/lib/converters/date-converter.ts
@@ -1,13 +1,9 @@
 import { JsonCustomConvert, JsonConverter } from 'json2typescript';
 
-// TODO: Investigate different formats that can be converted back and forth. perhaps allow some config
-// TODO: Factor in timezones
-
 @JsonConverter
 export class DateConverter implements JsonCustomConvert<Date> {
     serialize(date: Date): any {
-        // note: date.getMonth() returns a 0-index number
-        return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' +  date.getDate();
+        return date.toUTCString();
     }
     deserialize(date: any): Date {
         return new Date(date);
