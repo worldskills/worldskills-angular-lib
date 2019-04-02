f170e312f1a36e4334ab9da087a117f5ff902b7a
waseem.sabjee <waseem.sabjee@worldskills.org>
2019-04-02 10:55:44 +0200

    Breadcrumb testing

--- projects/worldskills-angular-lib/src/lib/breadcrumbs/breadcrumbs.component.spec.ts
+++ projects/worldskills-angular-lib/src/lib/breadcrumbs/breadcrumbs.component.spec.ts
@@ -1,21 +1,62 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, ComponentFixtureNoNgZone } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { BreadcrumbsComponent } from './breadcrumbs.component';
+import { Router, Routes } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  template: `Home`
+})
+class TestHomeComponent {
+}
+
+@Component({
+  template: `Other`
+})
+class TestOtherComponent {
+}
+
+@Component({
+  template: `OtherChild`
+})
+class TestOtherChildComponent {
+}
+
+
+@Component({
+  template: `<router-outlet #routerOutlet="outlet"></router-outlet><router-outlet name="popup"></router-outlet>`
+})
+export class TestAppComponent {
+}
+
 
 describe('BreadcrumbsComponent', () => {
   let component: BreadcrumbsComponent;
+  let router: Router;
   let fixture: ComponentFixture<BreadcrumbsComponent>;
+
+
+  const routes: Routes = [
+    { path: 'home', component: TestHomeComponent },
+    { path: 'other', component: TestOtherComponent, data: { breadcrumb: 'other' }, children: [
+      { path: 'child', component: TestOtherChildComponent, data: { breadcrumb: 'childs' } },
+      { path: 'child2', component: TestOtherChildComponent }
+    ] },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'popup', outlet: 'popup', component: TestOtherComponent, data: { breadcrumb: 'other' } }
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [ BreadcrumbsComponent ]
+      imports: [RouterTestingModule.withRoutes(routes)],
+      declarations: [ BreadcrumbsComponent, TestHomeComponent, TestOtherComponent, TestOtherChildComponent, TestAppComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    router = TestBed.get(Router);
     fixture = TestBed.createComponent(BreadcrumbsComponent);
     component = fixture.componentInstance;
     component.showHomeItem = true;
@@ -24,7 +65,34 @@ describe('BreadcrumbsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create - basic', () => {
+    router.initialNavigation();
+    expect(component).toBeTruthy();
+  });
+
+  it('should create - default redirect', () => {
+    router.navigate(['']);
+    expect(component).toBeTruthy();
+  });
+
+  it('should create - navigate', () => {
+    router.navigate(['/other']);
+    expect(component).toBeTruthy();
+  });
+
+  it('should create - navigate child', () => {
+    router.navigate(['/other/child']);
+    router.navigate(['other/child2']);
+    expect(component).toBeTruthy();
+  });
+
+  it('should create - navigate child with no breadcrumb', () => {
+    router.navigate(['other/child2']);
+    expect(component).toBeTruthy();
+  });
+
+  it('should create - navigate invalid route', () => {
+    router.navigate([{ outlets: { popup: ['popup'] } }]);
     expect(component).toBeTruthy();
   });
 });
