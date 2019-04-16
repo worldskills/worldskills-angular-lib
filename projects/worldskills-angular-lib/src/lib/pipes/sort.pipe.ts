0704acff8032d6e3a5b1d2a9d2c704f221c87f02
waseem.sabjee <waseem.sabjee@worldskills.org>
2019-04-16 09:22:43 +0200

    Updated sort pipe selector

--- projects/worldskills-angular-lib/src/lib/pipes/sort.pipe.ts
+++ projects/worldskills-angular-lib/src/lib/pipes/sort.pipe.ts
@@ -1,6 +1,6 @@
 import {Pipe, PipeTransform} from '@angular/core';
 @Pipe ({
-   name : 'sort'
+   name : 'wsSort'
 })
 export class SortPipe implements PipeTransform {
    transform(items, direction?, property?): any[] {
