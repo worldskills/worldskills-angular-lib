8f9ceb5336ff5c803dcdf604e518a91b3c32d30c
waseem.sabjee <waseem.sabjee@worldskills.org>
2019-10-15 13:47:17 +0200

    Resource Model entity property

--- projects/worldskills-angular-lib/src/lib/models/resource-model.ts
+++ projects/worldskills-angular-lib/src/lib/models/resource-model.ts
@@ -1,6 +1,7 @@
 import { I18nModel } from './I18n.model';
 import { LinkModelRaw } from './link-model-raw';
 import { ResourceMetadataModel } from './resource-metadata-model';
+import { WsEntityModel } from './ws-entity-model';
 
 // some fields are intentionally left out
 export class ResourceModel {
@@ -8,11 +9,15 @@ export class ResourceModel {
   name: I18nModel;
   metadata: ResourceMetadataModel[];
   links: LinkModelRaw[];
+  // tslint:disable-next-line:variable-name
+  ws_entity: number;
+
 
   constructor(obj?: any) {
     this.id = obj && obj.id || null;
     this.name = obj && obj.name || new I18nModel();
     this.metadata = obj && obj.metadata || [];
     this.links = obj && obj.links || [];
+    this.ws_entity = obj && obj.WsEntityModel || new WsEntityModel();
   }
 }
