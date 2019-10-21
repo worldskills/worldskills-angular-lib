203e3e69d479d3d7b528c6dc9a4f6656ebb14a03
waseem.sabjee <waseem.sabjee@worldskills.org>
2019-10-21 10:18:35 +0200

    FIX: version translations is an array

--- projects/worldskills-angular-lib/src/lib/models/version-model.ts
+++ projects/worldskills-angular-lib/src/lib/models/version-model.ts
@@ -8,14 +8,14 @@ export class VersionModel {
   date: string;
   // tslint:disable-next-line:variable-name
   modified_date: string;
-  translation: TranslastionModel;
+  translations: TranslastionModel[];
   tags: string[];
 
   constructor(obj?: Partial<VersionModel>) {
     this.id = obj && obj.id || null;
     this.tags = []; // ensure we don't create tags
     this.name = obj && obj.name || '';
-    this.translation = obj && obj.translation || new TranslastionModel();
+    this.translations = obj && obj.translations || [];
     this.date = obj && obj.date || DateUtil.toJavaDateString(new Date(), 'en');
     this.modified_date = obj && obj.modified_date || '';
   }
