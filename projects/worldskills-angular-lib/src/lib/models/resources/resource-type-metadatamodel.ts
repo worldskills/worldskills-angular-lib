3130002ec16b5322661605309933fa4f50f5d061
waseem.sabjee <waseem.sabjee@worldskills.org>
2019-10-23 10:16:08 +0200

    ResourceType properties

--- projects/worldskills-angular-lib/src/lib/models/resources/resource-type-metadatamodel.ts
+++ projects/worldskills-angular-lib/src/lib/models/resources/resource-type-metadatamodel.ts
@@ -1,9 +1,11 @@
+import { ResourceMetadataInfoModel } from './resource-metadata-info-model';
+
 export class ResourceTypeMetadataModel {
-  metadata: number;
+  metadata: ResourceMetadataInfoModel;
   required: boolean;
 
   constructor(obj?: any) {
-    this.metadata = obj && obj.metadata || null;
+    this.metadata = obj && obj.metadata || new ResourceMetadataInfoModel();
     this.required = obj && obj.required || false;
   }
 }
