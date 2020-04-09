1e9e39a457c90a0f267eb93d4d4c3e8f87729ca3
Fabian Vogler <fabian@equivalence.ch>
2020-04-09 14:59:35 +0200

    Update standard footer

--- projects/worldskills-angular-lib/src/lib/footer/footer.component.ts
+++ projects/worldskills-angular-lib/src/lib/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { UserModel } from '../models/user.model';
 
 @Component({
   selector: 'ws-footer',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  date;
 
-  constructor() { }
+  @Input() public isLoggedIn: boolean;
+  @Input() currentUser: UserModel;
+  @Output() public logoutClick: EventEmitter<any> = new EventEmitter();
+  @Output() public loginClick: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+    this.date = new Date();
+    this.currentUser = new UserModel();
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.loginClick.emit();
+  }
+
+  logout() {
+    this.logoutClick.emit();
   }
 
 }
