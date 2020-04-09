54cb22702591b055c5a66c8f087764ec866b2af6
waseem.sabjee <waseem.sabjee@worldskills.org>
2020-04-09 17:05:09 +0200

    Allow overriding of template columns

--- projects/worldskills-angular-lib/src/lib/footer/footer.component.ts
+++ projects/worldskills-angular-lib/src/lib/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, TemplateRef, ViewChild } from '@angular/core';
 import { UserModel } from '../models/user.model';
 
 @Component({
@@ -8,6 +8,22 @@ import { UserModel } from '../models/user.model';
 })
 export class FooterComponent implements OnInit {
   date;
+
+  @Input() col1Template: TemplateRef<any>;
+  @Input() col2Template: TemplateRef<any>;
+  @Input() col3Template: TemplateRef<any>;
+  @Input() col4Template: TemplateRef<any>;
+  @Input() col5Template: TemplateRef<any>;
+  @Input() col6Template: TemplateRef<any>;
+
+  @ViewChild('#col1DefaultTemplate')
+  col1DefaultTemplate: TemplateRef<any>;
+
+  @ViewChild('#col6DefaultTemplate')
+  col6DefaultTemplate: TemplateRef<any>;
+
+  @ViewChild('#colDefaultTemplate')
+  colDefaultTemplate: TemplateRef<any>;
 
   @Input() public isLoggedIn: boolean;
   @Input() currentUser: UserModel;
