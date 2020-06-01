370f512b3ef781d9958009bad3e7e774cf1e8988
waseem.sabjee <waseem.sabjee@worldskills.org>
2020-06-01 16:49:33 +0200

    FIX: grab time-value when changed in the text field  using the slash format

--- projects/worldskills-angular-lib/src/lib/models/datetime.model.ts
+++ projects/worldskills-angular-lib/src/lib/models/datetime.model.ts
@@ -32,10 +32,25 @@ export class DatetimeModel implements NgbDatetimeStruct {
   }
 
   public static fromLocalString(dateString: string): DatetimeModel {
+    dateString = dateString.trim();
+    const pieces = dateString.split(' ');
     let date: Date;
-    if (dateString.indexOf('/') > -1) {
-      const dateParts = dateString.split('/');
-      date = new Date(dateParts[2].substr(0, 4) + '-' + dateParts[1] + '-' + dateParts[0]);
+
+    if (pieces.length > 0) {
+      let dateParts = [];
+      let timeParts = [];
+      if (pieces[0].indexOf('/') > -1) {
+        dateParts = pieces[0].split('/');
+      }
+      if (pieces.length > 1) {
+        timeParts = pieces[1].split(':');
+      }
+      if (timeParts.length === 0) {
+        date = new Date(dateParts[2].substr(0, 4) + '-' + dateParts[1] + '-' + dateParts[0]);
+      } else {
+        date = new Date(dateParts[2].substr(0, 4) + '-' + dateParts[1] + '-' + dateParts[0] + ' ' + timeParts.join(':'));
+      }
+
     } else {
       date = new Date(dateString);
     }
