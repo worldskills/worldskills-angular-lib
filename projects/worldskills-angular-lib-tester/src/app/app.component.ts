f1378555edc135f02c80b9fb341ecc1a1f18fc20
waseem.sabjee <waseem.sabjee@worldskills.org>
2020-08-05 15:13:53 +0200

    Added redirectOrReturn example

--- projects/worldskills-angular-lib-tester/src/app/app.component.ts
+++ projects/worldskills-angular-lib-tester/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AlertService } from '../../../worldskills-angular-lib/src/lib/alerts/alert.service';
-import { AlertType, MenuItem, Language } from '../../../worldskills-angular-lib/src/public-api';
+import { AlertType, MenuItem, Language, Poll, Result, Vote } from '../../../worldskills-angular-lib/src/public-api';
 import { WorldskillsAngularLibService } from '../../../worldskills-angular-lib/src/lib/worldskills-angular-lib.service';
 import { NgAuthService } from '../../../worldskills-angular-lib/src/lib/auth/ng-auth.service';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { User } from '../../../../dist/worldskills-angular-lib/lib/auth/models/user';
+import { VoteEntry } from '../../../../dist/worldskills-angular-lib/lib/polls/models/vote-entry';
 
 @Component({
   selector: 'app-root',
@@ -26,6 +27,10 @@ export class AppComponent {
   defaultRoute = '/home';
 
   languages: Language[];
+
+  poll: Poll;
+  results: Result[];
+  voted: Vote;
 
   constructor(private alerts: AlertService, private wsi: WorldskillsAngularLibService, private oauth: OAuthService,
               private ngAuth: NgAuthService) {
@@ -56,6 +61,7 @@ export class AppComponent {
         console.log(error);
       }
     );
+    this.pollInit();
   }
 
   login(): void {
@@ -94,5 +100,81 @@ export class AppComponent {
     serviceConfig.apiEndpoint = 'http://localhost:8081';
     this.wsi.serviceConfigSubject.next(serviceConfig);
 
+  }
+
+  pollInit(): void {
+
+    this.poll = {
+      id: 1,
+      allowingReVote: true,
+      anonymousVoting: false,
+      anonymousResults: false,
+      showingResults: true,
+      created: new Date(),
+      deleted: false,
+      start: new Date(),
+      expiry: new Date(),
+      numberOfSelections: 3,
+      type: 'weighted',
+      question: { lang_code: 'en', text: 'Question ?' },
+      title: { lang_code: 'en', text: 'Title' },
+      options: [
+        { id: 1, deleted: false, text: {lang_code: 'en', text: 'Option 1'} },
+        { id: 2, deleted: false, text: {lang_code: 'en', text: 'Option 2'} },
+        { id: 3, deleted: false, text: {lang_code: 'en', text: 'Option 3'} },
+        { id: 4, deleted: false, text: {lang_code: 'en', text: 'Option 4'} }
+      ],
+      createdBy: {
+        id: 1,
+        firstName: 'Waseem',
+        lastName: 'Sabjee'
+      },
+      entity: {
+        id: 1,
+        name: { lang_code: 'en', text: 'WorldSkills International' }
+      },
+      entityPath: { pieces: [] }
+    };
+    this.poll.start.setDate(this.poll.start.getDate() - 3);
+    this.poll.expiry.setDate(this.poll.start.getDate() + 7);
+
+    this.results = [
+      {id: 1, count: 0, option: this.poll.options[0]},
+      {id: 2, count: 3, option: this.poll.options[1]},
+      {id: 3, count: 1, option: this.poll.options[2]},
+      {id: 4, count: 2, option: this.poll.options[3]}
+    ];
+
+    this.voted = {hasVoted: false, votes: []};
+  }
+
+  voteSelected(result: VoteEntry[]): void {
+    console.log(result);
+    this.voted.hasVoted = true;
+    this.voted.votes = result;
+    // this.selected.votes = this.voted.votes;
+  }
+
+  viewChange(view: string): void {
+    console.log(view);
+  }
+
+  voteRemoved(result: VoteEntry[]): void {
+    console.log(result);
+    this.voted.hasVoted = false;
+    this.voted.votes = [];
+    // this.selected = new AddVoteView();
+  }
+
+  edit(poll: Poll): void {
+    console.log('edit');
+  }
+
+  reset(poll: Poll): void {
+    console.log('reset');
+  }
+
+  delete(poll: Poll): void {
+    console.log('delete');
   }
 }
--- projects/worldskills-angular-lib-tester/src/app/home/home.component.ts
+++ projects/worldskills-angular-lib-tester/src/app/home/home.component.ts
@@ -4,6 +4,9 @@ import { Poll } from '../../../../worldskills-angular-lib/src/lib/polls/models/p
 import { Result } from '../../../../worldskills-angular-lib/src/lib/polls/models/result';
 import { Vote } from '../../../../worldskills-angular-lib/src/lib/polls/models/vote';
 import { VoteEntry } from '../../../../../dist/worldskills-angular-lib/lib/polls/models/vote-entry';
+import { NgAuthService } from '../../../../worldskills-angular-lib/src/lib/auth/ng-auth.service';
+import { Router } from '@angular/router';
+import { RedirectHandler } from '../../../../worldskills-angular-lib/src/lib/auth/handlers/redirect.handler';
 
 @Component({
   selector: 'app-home',
@@ -12,93 +15,15 @@ import { VoteEntry } from '../../../../../dist/worldskills-angular-lib/lib/polls
 })
 export class HomeComponent implements OnInit {
 
-  poll: Poll;
-  results: Result[];
-  voted: Vote;
-
   // selected: AddVoteView;
 
-  constructor() { }
+  constructor(protected service: NgAuthService, protected router: Router) { }
 
   // tslint:disable-next-line:typedef
   ngOnInit() {
-    this.init();
-  }
 
-  init(): void {
-
-    this.poll = {
-      id: 1,
-      allowingReVote: true,
-      anonymousVoting: false,
-      anonymousResults: false,
-      showingResults: true,
-      created: new Date(),
-      deleted: false,
-      start: new Date(),
-      expiry: new Date(),
-      numberOfSelections: 3,
-      type: 'weighted',
-      question: { lang_code: 'en', text: 'Question ?' },
-      title: { lang_code: 'en', text: 'Title' },
-      options: [
-        { id: 1, deleted: false, text: {lang_code: 'en', text: 'Option 1'} },
-        { id: 2, deleted: false, text: {lang_code: 'en', text: 'Option 2'} },
-        { id: 3, deleted: false, text: {lang_code: 'en', text: 'Option 3'} },
-        { id: 4, deleted: false, text: {lang_code: 'en', text: 'Option 4'} }
-      ],
-      createdBy: {
-        id: 1,
-        firstName: 'Waseem',
-        lastName: 'Sabjee'
-      },
-      entity: {
-        id: 1,
-        name: { lang_code: 'en', text: 'WorldSkills International' }
-      },
-      entityPath: { pieces: [] }
-    };
-    this.poll.start.setDate(this.poll.start.getDate() - 3);
-    this.poll.expiry.setDate(this.poll.start.getDate() + 7);
-
-    this.results = [
-      {id: 1, count: 0, option: this.poll.options[0]},
-      {id: 2, count: 3, option: this.poll.options[1]},
-      {id: 3, count: 1, option: this.poll.options[2]},
-      {id: 4, count: 2, option: this.poll.options[3]}
-    ];
-
-    this.voted = {hasVoted: false, votes: []};
-  }
-
-  voteSelected(result: VoteEntry[]): void {
-    console.log(result);
-    this.voted.hasVoted = true;
-    this.voted.votes = result;
-    // this.selected.votes = this.voted.votes;
-  }
-
-  viewChange(view: string): void {
-    console.log(view);
-  }
-
-  voteRemoved(result: VoteEntry[]): void {
-    console.log(result);
-    this.voted.hasVoted = false;
-    this.voted.votes = [];
-    // this.selected = new AddVoteView();
-  }
-
-  edit(poll: Poll): void {
-    console.log('edit');
-  }
-
-  reset(poll: Poll): void {
-    console.log('reset');
-  }
-
-  delete(poll: Poll): void {
-    console.log('delete');
+    const handler = new RedirectHandler(this.service, this.router);
+    handler.redirectOrReturn(['/nest'], error => console.log(error));
   }
 
 }
