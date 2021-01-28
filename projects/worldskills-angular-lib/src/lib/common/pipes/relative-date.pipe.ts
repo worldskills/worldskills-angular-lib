e208d9e25ff46001e45fd21d0557735a505d5f8a
waseem.sabjee <waseem.sabjee@worldskills.org>
2021-01-28 10:49:21 +0200

    implement releative date cconrol for voteee date range + support for future dates in relative date control

--- projects/worldskills-angular-lib/src/lib/common/pipes/relative-date.pipe.ts
+++ projects/worldskills-angular-lib/src/lib/common/pipes/relative-date.pipe.ts
@@ -2,11 +2,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { formatDate } from '@angular/common';
 // based of: http://www.angulartutorial.net/2017/10/how-to-show-date-is-today-or-yesterday.html
 
+/*
+ @Input() date: Date;
+  angDate: Date;
+  angDateValid: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.angDate = new Date(this.date);
+    this.angDateValid = this.angDate.toString() !== 'Invalid Date';
+  }
+*/
 @Pipe({
   name: 'relativeDate',
 })
 export class RelativeDatePipe implements PipeTransform {
   transform(value: Date, format?: string, locale?: string): string {
+    const dt =  new Date(value);
+    const isDateValid = dt.toString() !== 'Invalid Date';
+
+    if (!isDateValid) {
+      return formatDate(value, format, locale);
+    }
+
     format = format || 'MMM-dd-yyyy';
     locale = locale || 'en';
     const v = Number(value);
@@ -21,7 +40,13 @@ export class RelativeDatePipe implements PipeTransform {
 }
 
 function convertToNiceDate(date: Date): string {
-  const diff = (((new Date()).getTime() - date.getTime()) / 1000);
+  const now = new Date();
+  return date.getTime() < now.getTime() ? handlePastDate(date) : handleFutureDate(date);
+}
+
+function handlePastDate(date: Date): string {
+  const now = new Date();
+  const diff = ((now.getTime() - date.getTime()) / 1000);
   const daydiff = Math.floor(diff / 86400);
 
   if (isNaN(daydiff) || daydiff < 0 || daydiff >= 31) {
@@ -37,4 +62,24 @@ function convertToNiceDate(date: Date): string {
     daydiff === 1 && 'Yesterday' ||
     daydiff < 7 && daydiff + ' days ago' ||
     daydiff < 31 && Math.ceil(daydiff / 7) + ' week(s) ago';
+}
+
+function handleFutureDate(date: Date): string {
+  const now = new Date();
+  const diff = (( date.getTime() - now.getTime()) / 1000);
+  const daydiff = Math.floor(diff / 86400);
+
+  if (isNaN(daydiff) || daydiff < 0 || daydiff >= 31) {
+    return '';
+  }
+
+  return daydiff === 0 && (
+    diff < 60 && 'Just now' ||
+    diff < 120 && '1 minute from now' ||
+    diff < 3600 && Math.floor(diff / 60) + ' minutes from now' ||
+    diff < 7200 && '1 hour from now' ||
+    diff < 86400 && Math.floor(diff / 3600) + ' hours from now') ||
+    daydiff === 1 && 'Tomorrow' ||
+    daydiff < 7 && daydiff + ' days from now' ||
+    daydiff < 31 && Math.ceil(daydiff / 7) + ' week(s) from now';
 }
