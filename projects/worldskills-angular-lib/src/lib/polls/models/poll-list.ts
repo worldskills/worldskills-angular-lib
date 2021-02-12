291f3fbf593fed12e1c0f4b36eb4cf945e96248b
waseem.sabjee <waseem.sabjee@worldskills.org>
2021-02-12 10:15:02 +0200

    Ensure poll-list title is i18n

--- projects/worldskills-angular-lib/src/lib/polls/models/poll-list.ts
+++ projects/worldskills-angular-lib/src/lib/polls/models/poll-list.ts
@@ -1,9 +1,10 @@
 import { WsEntityModel } from '../../auth/models/ws-entity';
+import { I18nText } from '../../common/models/i18n-text';
 import { CreatedBy } from './created-by';
 
 export interface PollList {
     id: number;
-    title: string;
+    title: I18nText;
     entity: WsEntityModel;
     createdBy: CreatedBy;
     deleted: boolean;
