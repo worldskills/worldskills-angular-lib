58568f57fb1887b73a3ff3935a6931c86cd4b811
Waseem <waseemsabjee@gmail.com>
2025-12-11 11:41:36 +0200

    Update target path in language compile script

--- convert_lang_files.sh
+++ convert_lang_files.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Usage: ./convert_lang_file.sh <input.json> <object_name> [output.json.ts]
-./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/en.json' en './en.json.ts'
-./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/ar_AE.json' ar_AE './ar_AE.json.ts'
-./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/de.json' de './de.json.ts'
-./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/fi.json' fi './fi.json.ts'
-./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/fr.json' fr './fr.json.ts'
-./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/pt_BR.json' pt_BR './pt_BR.json.ts'
-./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/ru_RU.json' ru_RU './ru_RU.json.ts'
-./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/tt_RU.json' tt_RU './tt_RU.json.ts'
-./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/zh_CN.json' zh_CN './zh_CN.json.ts'
+./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/en.json' en './projects/worldskills-angular-lib/src/lib/i18n/en.json.ts'
+./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/ar_AE.json' ar_AE './projects/worldskills-angular-lib/src/lib/i18n/ar_AE.json.ts'
+./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/de.json' de './projects/worldskills-angular-lib/src/lib/i18n/de.json.ts'
+./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/fi.json' fi './projects/worldskills-angular-lib/src/lib/i18n/fi.json.ts'
+./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/fr.json' fr './projects/worldskills-angular-lib/src/lib/i18n/fr.json.ts'
+./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/pt_BR.json' pt_BR './projects/worldskills-angular-lib/src/lib/i18n/pt_BR.json.ts'
+./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/ru_RU.json' ru_RU './projects/worldskills-angular-lib/src/lib/i18n/ru_RU.json.ts'
+./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/tt_RU.json' tt_RU './projects/worldskills-angular-lib/src/lib/i18n/tt_RU.json.ts'
+./convert_lang_file.sh './projects/worldskills-angular-lib/assets/i18n/lazy-loaded/zh_CN.json' zh_CN './projects/worldskills-angular-lib/src/lib/i18n/zh_CN.json.ts'
 
 
 
 
 
+ 
