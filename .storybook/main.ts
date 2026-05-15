1ed45777f3ef548f6fbefc7509cc8a29b235a84d
Waseem <waseemsabjee@gmail.com>
2026-05-15 09:32:02 +0200

    cleanup and fixes of the workspace

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,10 +5,6 @@ const config: StorybookConfig = {
   stories: [
     '../projects/worldskills-ng-tester-prime/src/**/*.stories.@(ts|mdx)',
   ],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
   framework: {
     name: '@storybook/angular',
     options: {},
